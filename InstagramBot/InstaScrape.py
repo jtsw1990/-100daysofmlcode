1b68f85c19de75fad067a35b1f85685ce9b4d68a
Jtsw <jtsw1990@gmail.com>
2018-12-11 08:40:54 +1100

    Add files via upload

--- InstagramBot/InstaScrape.py
+++ InstagramBot/InstaScrape.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import random
+import numpy as np
 
 class InstagramBot(object):
 
@@ -21,10 +22,10 @@ class InstagramBot(object):
 
         driver = self.driver
         driver.get("https://www.instagram.com/")
-        time.sleep(3)
+        time.sleep(np.random.randint(2, 4))
         login_button = driver.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
         login_button.click()
-        time.sleep(3)
+        time.sleep(np.random.randint(2, 4))
         user_name_elem = driver.find_element_by_xpath("//input[@name='username']")
         user_name_elem.clear()
         user_name_elem.send_keys(self.username)
@@ -32,19 +33,19 @@ class InstagramBot(object):
         password_elem.clear()
         password_elem.send_keys(self.password)
         password_elem.send_keys(Keys.RETURN)
-        time.sleep(3)
+        time.sleep(np.random.randint(2, 4))
 
     def like_photo(self, hashtag):
         driver = self.driver
         driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
-        time.sleep(2)
+        time.sleep(10)
 
         # gathering photos
         pic_hrefs = []
         for i in range(1, 7):
             try:
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                time.sleep(2)
+                time.sleep(np.random.randint(5, 10))
                 # get tags
                 hrefs_in_view = driver.find_elements_by_tag_name('a')
                 # finding relevant hrefs
@@ -60,10 +61,10 @@ class InstagramBot(object):
         unique_photos = len(pic_hrefs)
         for pic_href in pic_hrefs:
             driver.get(pic_href)
-            time.sleep(2)
+            time.sleep(np.random.randint(5, 10))
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             try:
-                time.sleep(random.randint(2, 4))
+                time.sleep(random.randint(5, 10))
                 like_button = lambda: driver.find_element_by_xpath('//span[@aria-label="Like"]').click()
                 like_button().click()
                 for second in reversed(range(0, random.randint(18, 28))):
@@ -71,18 +72,19 @@ class InstagramBot(object):
                                     + " | Sleeping " + str(second))
                     time.sleep(1)
             except Exception as e:
-                time.sleep(2)
+                time.sleep(np.random.randint(5, 10))
             unique_photos -= 1
 
 if __name__ == '__main__':
-    username = 'test1'
-    password = 'test1'
+    username = 'jtsw1990'
+    password = 'b0mber'
 
     ig = InstagramBot(username, password)
     ig.login()
     while True:
-        hashtags = ['code', 'machinelearning', 'actuary', 'actuaries',
-                'artificialintelligence', 'computer', 'coding', 'python', 'programming', 'javascript', 'js',
-                'html', 'css']
+        hashtags = ['python', 'machinelearning', 'algorithm', 'artificialintelligence',
+        'ai', '100daysofmlcode', 'mathematics', 'statistics', 'actuary', 'actuarial',
+        'statistics', 'html', 'css', 'javascript', 'atom', 'datascience', 'analytics',
+        'bigdata', 'code', 'coding', 'jupyter']
         tag = random.choice(hashtags)
         ig.like_photo(tag)
